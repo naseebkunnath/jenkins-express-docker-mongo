b76cbc02c8eafb872e1989cae0255aff30c9dc59
Naseeb Rahman <naseebrahmantech@gmail.com>
2025-07-13 18:45:11 +0530

    small code change test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 //C:\Program Files\Common Files\Oracle\Java\javapath
-app.get('/', (req, res) => res.send('Hello from CI/CD! This is Naseeb'));
+app.get('/', (req, res) => res.send('Hello from CI/CD! This is Naseeb Rahman'));
 
 app.listen(3000, () => console.log('Server running on port 3000'));
